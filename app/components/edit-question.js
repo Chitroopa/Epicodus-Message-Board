f064fd6b3f704f468391cfb96f438c0f9ae6e088
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-06-02 13:52:30 -0700

    Add delete question functionality

---
+++ app/components/edit-question.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    editQuestionFormShow() {
+      this.set('editQuestion', true);
+    },
+    deleteQuestion1(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('deleteQuestion2', question);
+      }
+    },
+    updateQuestion1(question) {
+      if (this.get('content') !== undefined)
+      {
+        var params = {
+          title: this.get('title'),
+          content: this.get('content'),
+          author: this.get('author'),
+          notes: this.get('notes'),
+          shortContent : (this.get('content').slice(0, 100)).concat("..."),
+        }
+      }
+      else
+      {
+        var params = {
+          title: this.get('title'),
+          content: this.get('content'),
+          author: this.get('author'),
+          notes: this.get('notes'),
+          shortContent : this.get('content'),
+        }
+      }
+
+
+      console.log(params);
+      this.set('editQuestion', false);
+      this.sendAction('updateQuestion2', question, params);
+
+      this.set('title', '');
+      this.set('content', '');
+      this.set('author', '');
+      this.set('notes', '');
+    }
+  }
+});
--- app/routes/question.js
+++ app/routes/question.js
@@ -14,6 +14,24 @@ export default Ember.Route.extend({
         return question.save();
       });
       this.transitionTo('index');
+    },
+    updateQuestion3(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined && params[key]!=="" && params[key]!=="..." ) {
+          question.set(key,params[key]);
+        }
+      });
+     question.save();
+     this.transitionTo('question');
+   },
+   deleteQuestion3(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
     }
   }
 });
