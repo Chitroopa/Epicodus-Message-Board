c559fdcf56abadec5b6e51f997fcbc9160f12932
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-06-02 16:47:20 -0700

    remove comments

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     saveAnswer1() {
-      var regex = "/\n/g"
       var params = {
         answer: $('#answer').val(),
         // answer: this.get('answer'),
